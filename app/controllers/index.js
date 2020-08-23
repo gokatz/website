79c7e97d28edba8b80862ba9a1394fb5a9e5db8b
gokul.kathirvel <gokul.kathirvel@zohocorp.com>
2020-08-23 08:52:31 +0530

    speaker list

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,8 @@
 import Controller from '@ember/controller';
 import previousTalksList from '../data/previous-talks';
+import speakers from '../data/speakers';
 
 export default class IndexController extends Controller {
+  speakers = speakers;
   previousTalksList = previousTalksList
 }
